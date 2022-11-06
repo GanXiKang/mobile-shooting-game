dbab15bb80c148625a0f2d173133cc0db9a6bbbd
GanXiKang <gan0612xk@hotmail.com>
2022-11-06 13:36:56 +0800

    feat：添加敵人移動

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    public float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,19 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer++;
+
+        if (timer < 50f) 
+        {
+            transform.position = new Vector3(transform.position.x + 3f * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if (timer > 50f && timer < 100f)
+        {
+            transform.position = new Vector3(transform.position.x - 3f * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if (timer > 100f) 
+        {
+            timer = 0;
+        }
     }
 }
