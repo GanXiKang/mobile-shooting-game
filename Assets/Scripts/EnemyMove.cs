fb67928583727cc8e2a3805b12f01aa408270a06
GanXiKang <gan0612xk@hotmail.com>
2022-11-06 14:16:20 +0800

    feat：添加敵人移動時面朝去的方向

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -16,15 +16,23 @@ public class EnemyMove : MonoBehaviour
     {
         timer++;
 
-        if (timer < 50f) 
+        if (timer < 500f) 
         {
             transform.position = new Vector3(transform.position.x + 3f * Time.deltaTime, transform.position.y, transform.position.z);
+            
+            // 使用 Lerp 漸漸轉向
+            Quaternion targetRotation = Quaternion.Euler(0, 90, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.3f);
         }
-        if (timer > 50f && timer < 100f)
+        if (timer > 500f && timer < 1000f)
         {
             transform.position = new Vector3(transform.position.x - 3f * Time.deltaTime, transform.position.y, transform.position.z);
+
+            // 使用 Lerp 漸漸轉向
+            Quaternion targetRotation = Quaternion.Euler(0, 270, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.3f);
         }
-        if (timer > 100f) 
+        if (timer > 1000f) 
         {
             timer = 0;
         }
