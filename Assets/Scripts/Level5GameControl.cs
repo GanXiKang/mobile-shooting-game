d280d187efdde2e1171dcb735019e1078239f319
GanXiKang <gan0612xk@hotmail.com>
2022-11-08 04:07:59 +0800

    feat：添加第5關GameControl

--- Assets/Scripts/Level5GameControl.cs
+++ Assets/Scripts/Level5GameControl.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Level4GameControl : MonoBehaviour
+public class Level5GameControl : MonoBehaviour
 {
+
+
+    // Update is called once per frame
     void Update()
     {
         GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
 
         if (enemys.Length == 0)
         {
-            SceneManager.LoadScene(4);
+            SceneManager.LoadScene(0);
         }
         if (Input.GetKey("f1"))
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(4);
         }
     }
 }
