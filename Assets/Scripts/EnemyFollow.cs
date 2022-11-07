8c27cd36aee232b0105dd5a90747bc4b6763c007
GanXiKang <gan0612xk@hotmail.com>
2022-11-08 01:08:09 +0800

    feat: 添加敵人跟隨，完成第4關場景

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 5f;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        var tr = Quaternion.LookRotation(target.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(tr, transform.rotation, speed * Time.deltaTime);
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
