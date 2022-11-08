8e367aa9c0352c11ed2099d2232c6215733724ed
GanXiKang <gan0612xk@hotmail.com>
2022-11-08 21:06:50 +0800

    feat： 添加數值

--- Assets/Scripts/Level0Start.cs
+++ Assets/Scripts/Level0Start.cs
@@ -8,5 +8,7 @@ public class Level0Start : MonoBehaviour
     public void Click() 
     {
         SceneManager.LoadScene(1);
+        HeartControl.heart = 100;
+        CoinScore.Score = 0;
     }
 }
