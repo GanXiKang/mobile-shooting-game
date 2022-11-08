15b2459640a584c76240f6b67e00c898acde5267
GanXiKang <gan0612xk@hotmail.com>
2022-11-08 16:08:26 +0800

    feat ： 添加失敗和重開游戲的UI

--- Assets/Scripts/HeartControl.cs
+++ Assets/Scripts/HeartControl.cs
@@ -7,9 +7,17 @@ public class HeartControl : MonoBehaviour
 {
     public static float heart = 100;
     public Text heartText;
+    public GameObject LoseText;
+    public GameObject RestartText;
 
     void Update()
     {
         heartText.text = heart.ToString();
+
+        if (heart <= 0) 
+        {
+            LoseText.SetActive(true);
+            RestartText.SetActive(true);
+        }
     }
 }
