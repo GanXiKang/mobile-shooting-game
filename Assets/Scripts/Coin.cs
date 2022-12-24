d7a321bfa48339e236770e0ab92575a24b31f040
GanXiKang <gan0612xk@hotmail.com>
2022-12-24 19:10:20 +0800

    feat ： 開寶箱的錢幣優化

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private float Timer = 0;
     void Update()
     {
+        Timer++;
+        if (Timer < 200f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
+            Timer = 0;
+        }
         transform.Rotate(0, 45f * Time.deltaTime, 0);
     }
     private void OnTriggerEnter(Collider other)
